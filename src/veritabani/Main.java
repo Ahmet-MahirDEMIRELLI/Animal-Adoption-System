619138bf1d53b945f4075d9458e42c2cea87cac3
Ahmet-MahirDEMIRELLI <a.mahir.demirelli@gmail.com>
2024-09-15 15:27:56 +0300

    Initial Commit

---
+++ src/veritabani/Main.java
@@ -0,0 +1,20 @@
+package veritabani;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+		String databaseName = "VeritabaniProje";
+		String userName = "postgres";
+		String password = "12345";
+		Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + databaseName, userName,
+				password);
+
+		StartPage startPage = new StartPage(conn);
+		startPage.showFrame();
+	}
+}
